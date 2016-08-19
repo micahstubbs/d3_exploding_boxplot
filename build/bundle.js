c0d1fad518a29d8e6611ba3b0a44e0a4023b3c5b
Micah Stubbs <micah.stubbs@gmail.com>
2016-08-19 10:34:40 -0700

    abstract out computeBoxplot

--- build/bundle.js
+++ build/bundle.js
@@ -212,6 +212,35 @@
     }
   }
 
+  function computeBoxplot(data, iqrScalingFactor, value) {
+    console.log('computeBoxplot() was called');
+    iqrScalingFactor = iqrScalingFactor || 1.5;
+    value = value || Number;
+    var seriev = data.map(function (m) {
+      return m[value];
+    }).sort(d3.ascending);
+    var quartiles = [d3.quantile(seriev, 0.25), d3.quantile(seriev, 0.5), d3.quantile(seriev, 0.75)];
+    var iqr = (quartiles[2] - quartiles[0]) * iqrScalingFactor;
+    // separate outliers
+    var max = Number.MIN_VALUE;
+    var min = Number.MAX_VALUE;
+    var boxData = d3.nest().key(function (d) {
+      var v = d[value];
+      var type = v < quartiles[0] - iqr || v > quartiles[2] + iqr ? 'outlier' : 'normal';
+      if (type === 'normal' && (v < min || v > max)) {
+        max = Math.max(max, v);
+        min = Math.min(min, v);
+      }
+      return type;
+    }).map(data);
+    if (!boxData.outlier) boxData.outlier = [];
+    boxData.quartiles = quartiles;
+    boxData.iqr = iqr;
+    boxData.max = max;
+    boxData.min = min;
+    return boxData;
+  };
+
   function d3_exploding_boxplot () {
     // options which should be accessible via ACCESSORS
     var dataSet = [];
@@ -664,42 +693,38 @@
 
     // END ACCESSORS
 
-    var computeBoxplot = function computeBoxplot(data, iqrScalingFactor, value) {
-      console.log('computeBoxplot() was called');
-      iqrScalingFactor = iqrScalingFactor || 1.5;
-      value = value || Number;
-
-      var seriev = data.map(function (m) {
-        return m[value];
-      }).sort(d3.ascending);
-
-      var quartiles = [d3.quantile(seriev, 0.25), d3.quantile(seriev, 0.5), d3.quantile(seriev, 0.75)];
-
-      var iqr = (quartiles[2] - quartiles[0]) * iqrScalingFactor;
-
-      // separate outliers
-      var max = Number.MIN_VALUE;
-      var min = Number.MAX_VALUE;
-      var boxData = d3.nest().key(function (d) {
-        var v = d[value];
-        var type = v < quartiles[0] - iqr || v > quartiles[2] + iqr ? 'outlier' : 'normal';
-        if (type === 'normal' && (v < min || v > max)) {
-          max = Math.max(max, v);
-          min = Math.min(min, v);
-        }
-        return type;
-      }).map(data);
-
-      if (!boxData.outlier) boxData.outlier = [];
-
-      boxData.quartiles = quartiles;
-
-      boxData.iqr = iqr;
-      boxData.max = max;
-      boxData.min = min;
-
-      return boxData;
-    };
+    // let computeBoxplot = (data, iqrScalingFactor, value) => {
+    //   console.log('computeBoxplot() was called');
+    //   iqrScalingFactor = iqrScalingFactor || 1.5;
+    //   value = value || Number;
+    //   const seriev = data.map(m => m[value]).sort(d3.ascending);
+    //   const quartiles = [
+    //     d3.quantile(seriev, 0.25),
+    //     d3.quantile(seriev, 0.5),
+    //     d3.quantile(seriev, 0.75)
+    //   ];
+    //   const iqr = (quartiles[2] - quartiles[0]) * iqrScalingFactor;
+    //    // separate outliers
+    //   let max = Number.MIN_VALUE;
+    //   let min = Number.MAX_VALUE;
+    //   const boxData = d3.nest()
+    //       .key(d => {
+    //         const v = d[value];
+    //         const type = (v < quartiles[0] - iqr || v > quartiles[2] + iqr) ? 'outlier' : 'normal';
+    //         if (type === 'normal' && (v < min || v > max)) {
+    //           max = Math.max(max, v);
+    //           min = Math.min(min, v);
+    //         }
+    //         return type;
+    //       })
+    //       .map(data);
+    //   if (!boxData.outlier) boxData.outlier = [];
+    //   boxData.quartiles = quartiles;
+    //   boxData.iqr = iqr;
+    //   boxData.max = max;
+    //   boxData.min = min;
+    //   return boxData;
+    // };
 
     return chart;
   }
---
+++ src/computeBoxplot.js
@@ -0,0 +1,32 @@
+export function computeBoxplot(data, iqrScalingFactor, value) {
+  console.log('computeBoxplot() was called');
+  iqrScalingFactor = iqrScalingFactor || 1.5;
+  value = value || Number;
+  const seriev = data.map(m => m[value]).sort(d3.ascending);
+  const quartiles = [
+    d3.quantile(seriev, 0.25),
+    d3.quantile(seriev, 0.5),
+    d3.quantile(seriev, 0.75)
+  ];
+  const iqr = (quartiles[2] - quartiles[0]) * iqrScalingFactor;
+   // separate outliers
+  let max = Number.MIN_VALUE;
+  let min = Number.MAX_VALUE;
+  const boxData = d3.nest()
+      .key(d => {
+        const v = d[value];
+        const type = (v < quartiles[0] - iqr || v > quartiles[2] + iqr) ? 'outlier' : 'normal';
+        if (type === 'normal' && (v < min || v > max)) {
+          max = Math.max(max, v);
+          min = Math.min(min, v);
+        }
+        return type;
+      })
+      .map(data);
+  if (!boxData.outlier) boxData.outlier = [];
+  boxData.quartiles = quartiles;
+  boxData.iqr = iqr;
+  boxData.max = max;
+  boxData.min = min;
+  return boxData;
+};
